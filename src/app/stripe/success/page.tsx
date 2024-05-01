bf36d374029455bc9841e5988cc29eda6f67472d
mrsaadchohan <saadchohan278@gmail.com>
2024-05-01 22:38:02 +0500

    Fix unescaped double quotes in StripeSuccess page

--- src/app/stripe/success/page.tsx
+++ src/app/stripe/success/page.tsx
@@ -8,17 +8,17 @@ const StripeSuccess = () => {
     <div className='mt-12 md:max-w-[50vw] mx-auto'>
       <CheckCheck className='text-green-600 w-16 h-16 mx-auto my-6'/>
       <div className='text-center'>
-    <h3 className='md:text-2xl text-base text-gray-900 font-semibold'>Payment Done!</h3>
-    <p className="text-gray-600 my-2">
-    "Thank you for your purchase. We hope you enjoy it!"
-          </p>
-          <p>Have a great day!</p>
+        <h3 className='md:text-2xl text-base text-gray-900 font-semibold'>Payment Done!</h3>
+        <p className="text-gray-600 my-2">
+          {"Thank you for your purchase. We hope you enjoy it!"}
+        </p>
+        <p>Have a great day!</p>
 
-          <Button asChild className='mt-5'>
-            <Link href={'/'}>
-                Go back
-            </Link>
-          </Button>
+        <Button asChild className='mt-5'>
+          <Link href={'/'}>
+            Go back
+          </Link>
+        </Button>
       </div>
     </div>
   )
